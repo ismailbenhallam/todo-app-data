c52cc933186aaef16a0d664038e40502a5100756
Ismaïl BENHALLAM <ismailben44@gmail.com>
2022-02-19 18:40:26 +0100

    ✨ Add script to delete all todos

---
+++ scripts/delete_all.py
@@ -0,0 +1,24 @@
+"""Script to drop all TODOs from the DB"""
+
+import inquirer
+
+from constants import TABLE_NAME
+from logger import get_logger
+from todos_db_connection import get_connection
+
+
+def delete_all():
+    engine = get_connection()
+
+    engine.execute(f"DELETE FROM {TABLE_NAME}")
+
+
+if __name__ == "__main__":
+    answers = inquirer.prompt(
+        [inquirer.Confirm("confirm", message="You are about deleting all TODOs from the DB. Are you sure ?")],
+    )
+
+    if answers["confirm"]:
+        delete_all()
+        log = get_logger(__name__)
+        log.info("All TODOs where deleted")
